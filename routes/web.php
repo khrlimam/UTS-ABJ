127611ced71c3b517ca159e8e2fa2de6abba3298
Khairul Imam <ki65559@gmail.com>
2019-05-24 13:16:52 +0800

    show fail message

--- routes/web.php
+++ routes/web.php
@@ -7,8 +7,6 @@ Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
 
-
-
 Route::prefix('ospf')->middleware('auth')->group(function() {
     Route::resource('ospf', 'OspfController');
 });
